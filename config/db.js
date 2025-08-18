a51487b35985e0636a0136e80de85d8a99ba5cf7
Your Name <gauravmore33444@gmail.com>
2025-08-18 19:56:03 +0530

    some changes after deployment

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-const pool = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
